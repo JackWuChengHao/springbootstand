ac4e78a2d4d246e0e5fd3dea6ccbe4a00bc85fae
WuChenghao <WuChenghao@DESKTOP-B6AA7HD>
2018-08-08 09:35:27 +0800

    初始化实验项目

---
+++ src/main/java/xiaowu/lab/springboottemplate/App.java
@@ -0,0 +1,13 @@
+package xiaowu.lab.springboottemplate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
